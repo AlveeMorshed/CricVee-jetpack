0cb05cba2da679ce7d11e14273b3440839cfd415
AlveeMorshed <alvee2015@gmail.com>
2024-04-17 16:09:44 +0600

    [CricVee-jetpack] Minor Adjustments to MatchCard UI and added some functions to fetch teams.

--- app/src/main/java/com/alvee/cricvee_jetpack/domain/repository/FixtureRepository.kt
+++ app/src/main/java/com/alvee/cricvee_jetpack/domain/repository/FixtureRepository.kt
@@ -1,13 +1,11 @@
 package com.alvee.cricvee_jetpack.domain.repository
 
-import com.alvee.cricvee_jetpack.data.db.model.teams.TeamData
 import com.moinul.cricvee.model.fixtures.FixtureData
 import kotlinx.coroutines.flow.Flow
 
 interface FixtureRepository {
 
     suspend fun insertAllFixtures(fixtureList: List<FixtureData>)
-    suspend fun insertAllTeams(teamsList: List<TeamData>)
 
     fun readRecentFixtureData(): Flow<List<FixtureData>>
 }
